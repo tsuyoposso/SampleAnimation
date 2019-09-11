dd75875501374034b21bae5a65e4ac2d2a2da4f2
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-09-11 14:18:47 +0900

    サンプルアニメーションをつけた

--- SampleAnimation/SampleAnimation/ViewController.swift
+++ SampleAnimation/SampleAnimation/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
+    
+    @IBAction func tapStartButton(_ sender: Any) {
+        //ビューの角を丸める処理をアニメーションで表現
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        // 変更前の値
+        animation.fromValue = 0
+        // 変更後の値
+        animation.toValue = 50
+        // アニメーションの時間 second
+        animation.duration = 3
+        // アニメーションをレイヤーに追加する
+        targetView.layer.add(animation, forKey: "cornerRadius")
+        // 変更後も角丸を維持
+        targetView.layer.cornerRadius = 50
+        
+        // クロージャを用いた書き方。記載がシンプルになる
+        self.targetView.alpha = 1
+        UIView.animate(withDuration: 5.0) {
+            self.targetView.alpha = 0
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
